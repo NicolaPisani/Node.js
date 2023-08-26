cf40f83cae435e4209cd74dfbcefe70198853f66
Nicola Pisani <pisani_nicola@yahoo.com>
2023-08-26 16:55:56 +0200

    esercizio-2

---
+++ es2-import.js
@@ -0,0 +1,7 @@
+const sayHello = require("./es2"); // Nota: './greetings' indica il percorso del modulo locale
+
+const saluti = sayHello("Nico");
+console.log(saluti);
+
+
+//Importo la funzione sayHello dal modulo es2.js , Poi ho chiamato la funzione passando il nome 'Nico' e ho stampato il risultato sulla console.
---
+++ es2.js
@@ -0,0 +1,5 @@
+function sayHello(name) {
+  return `Ciao, ${name}!`;
+}
+
+module.exports = sayHello;
